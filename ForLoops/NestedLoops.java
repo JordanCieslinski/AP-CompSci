4ffdbc3ae628e0a255d71375c01bf83d6b3916b9
JordanCieslinski <jordan@cieslinskis.com>
2021-10-04 16:44:09 -0400

    10/4

--- ForLoops/NestedLoops.java
+++ ForLoops/NestedLoops.java
@@ -1,35 +1,63 @@
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++)
+                System.out.print("*");
+            System.out.println();
+        }
 
-public class NestedLoops
-{
-    public static void main(String[] args)
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 5; j++)
-                System.out.print("*");
-            System.out.println();
-        }
-        
         System.out.println();
-        
-        for(int i = 5; i >= 0; i--)
-        {
-            for(int j = i+1; j <= 5; j++)
-            {
+
+        for (int i = 5; i >= 0; i--) {
+            for (int j = i + 1; j <= 5; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        
+
         System.out.println();
-        
-        for(int i = 1; i <= 7; i++)
-        {
-            for(int j = i+1; j >= 8 ; j++)
-            {
-                System.out.print(i);
+
+        int count = 0;
+
+        for (int i = 7; i >= 1; i--) {
+            count++;
+            for (int j = i + 1; j <= 8; j++) {
+                System.out.print(count);
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        for (int i = 1; i <= 3; i++) {
+
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 1; k <= 3; k++) {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        int num = 0;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 1; j <= i+1; j++) {
+
+                num = (int) Math.pow(10, i);
+
+                System.out.print(num + "");
+
+            }
+            System.out.println();
+
+
+        }
+
+
     }
+
 }
