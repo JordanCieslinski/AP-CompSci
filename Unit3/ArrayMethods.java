3a95702b0ba1237c981c69ee4544f75b8f143fc2
JordanCieslinski <jordan@cieslinskis.com>
2021-11-01 12:13:54 -0400

    .

--- Unit3/ArrayMethods.java
+++ Unit3/ArrayMethods.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class ArrayMethods {
     public static void main(String [] args){
-        int [] arr = {9,0,9,9};
-        System.out.println(lastIndexOf(arr, 9));
+        String [] arr = {"Hello","Hey","Hi"};
+        System.out.println(findLongestWord(arr));
     }
     public static int lastIndexOf(int[] arr , int x){
         int loc = arr.length-1;
@@ -19,4 +19,40 @@ public class ArrayMethods {
         Arrays.sort(arr);
         return arr[arr.length-1] - arr[0];
     }
+    public static int countInRange(int[] arr, int min, int max){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < max && arr[i] > min)
+                count++;
+        }
+        return count;
+    }
+    public static int oneLessSum(int [] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length-1; i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static int[] swapPlaces(int[] arr) {
+        int temp = arr[arr.length-1];
+        arr[arr.length-1] = arr[0];
+        arr[0] = temp;
+        return arr;
+      }
+    public static String findLongestWord(String [] arr){
+        int biggest = 0;
+        int pos = 0;
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i].length() > biggest){
+                biggest = arr[i].length();
+                pos = i;
+            }
+        }
+        return arr[pos];
+    }
+
+
+
 }
+
