d2a33ce08ab3dfeb8a338fc558f541b1ed018383
JordanCieslinski <56612682+JordanCieslinski@users.noreply.github.com>
2021-12-13 11:07:23 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,87 @@
+public class Student
+{
+    private String name;
+    private int test1;
+    private int test2;
+    private int test3;
+    private static int studentCounter;
+    
+    //Default Constructor: Set name to Caleb and test1 to 50, update studentCounter by 1
+    public Student()
+    {
+        
+    }
+    //Overloaded Constructor: Set all the fields to their respective parameters, update studentCounter by 1
+    public Student(String n, int t1, int t2, int t3)
+    {
+        
+    }
+    //Accessor method because it accesses a field
+    public String getName()
+    {
+        
+    }
+    /**
+     * pre: needs 1 <= testNum <= 3
+     * post: returns the specified grade
+     */
+    public int getScore(int testNum)
+    {
+        
+    }
+    
+    public static int getStudentCounter()
+    {
+        
+    }
+    //Mutator method
+    public void setName(String nm)
+    {
+        
+    }
+    //Returns: _____ has three test scores of: ___ , ___ , ___
+    public String toString()
+    {
+        
+    }
+    //Returns the student's test average
+    public double getAverage()
+    {
+        
+    }
+    //Returns the highest test score
+    public int getHighScore()
+    {
+        
+    }
+    //Returns the true if this Student has a higher average than ben
+    public boolean hasHigherTestAverage(Student ben)
+    {
+        
+    }
+    //Returns the Student with the higher test average
+    public Student getBetterStudent(Student mike)
+    {
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
