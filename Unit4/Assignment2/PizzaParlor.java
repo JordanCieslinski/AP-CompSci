700bd243712aa4e6571433ce0be5016c2fd7e099
JordanCieslinski <jordan@cieslinskis.com>
2022-01-05 16:13:55 -0500

    .

--- Unit4/Assignment2/PizzaParlor.java
+++ Unit4/Assignment2/PizzaParlor.java
@@ -5,12 +5,14 @@ public class PizzaParlor {
     private int myNumPeppPizzas;
     private int myNumVegPizzas;
     private int myCheeseSupply;
-    private int myPepperoniSupply; 
+    private int myPepperoniSupply;
     private int myVeggieSupply;
     private int myRevenue;
     private int myOrigAcctBal;
 
-    public PizzaParlor(){
+    private int myDoughSupply;
+
+    public PizzaParlor() {
         myNumCheesePizzas = 0;
         myNumPeppPizzas = 0;
         myNumVegPizzas = 0;
@@ -19,57 +21,70 @@ public class PizzaParlor {
         myVeggieSupply = 200;
         myRevenue = 0;
         myOrigAcctBal = 1000;
-    }
-    public void orderCheese(){
-        myNumCheesePizzas++;
-        myCheeseSupply -= 12;
-        myRevenue+=8;
+
+        myDoughSupply = 400;
     }
 
-    public void orderPepperoni(){
+    public void orderCheese() {
+        myNumCheesePizzas++;
+        myCheeseSupply -= 12;
+        myRevenue += 8;
+        myDoughSupply -=11;
+    }
+
+    public void orderPepperoni() {
         myNumPeppPizzas++;
         myCheeseSupply -= 8;
         myPepperoniSupply -= 6;
         myRevenue += 10;
 
+        myDoughSupply -=11;
+
     }
 
-    public void orderVeggie(){
+    public void orderVeggie() {
         myNumPeppPizzas++;
         myCheeseSupply -= 8;
         myVeggieSupply -= 12;
         myRevenue += 11;
 
+        myDoughSupply -=11;
+
     }
 
-    public int getCheesePizzas(){
+    public int getCheesePizzas() {
         return myNumCheesePizzas;
     }
 
-    public int getPeppPizzas(){
+    public int getPeppPizzas() {
         return myNumPeppPizzas;
     }
 
-    public int getVegPizzas(){
+    public int getVegPizzas() {
         return myNumVegPizzas;
     }
 
-    public int getCheeseSupply(){
+    public int getCheeseSupply() {
         return myCheeseSupply;
     }
 
-    public int getPepperoniSupply(){
+    public int getPepperoniSupply() {
         return myPepperoniSupply;
     }
 
-    public int getVeggieSupply(){
+    public int getVeggieSupply() {
         return myVeggieSupply;
     }
 
-    public int getMyRevenue(){
+    public double getMyRevenue() {
         return myRevenue;
     }
-    public int getMyOrigAcctBal(){
+
+    public double getMyOrigAcctBal() {
         return myOrigAcctBal;
+    }
+
+    public int getMyDoughSupply(){
+        return myDoughSupply;
     }
 }
--- Unit4/Assignment2/PizzaTester.java
+++ Unit4/Assignment2/PizzaTester.java
@@ -1,30 +1,36 @@
 package Assignment2;
+import java.util.*;
 
-public class PizzaTester {
-    public static void main(String[] args) {
-        PizzaParlor jimboslice = new PizzaParlor();
-        orderPizzas(jimboslice, 5, 3, 2);
-        printOrder(jimboslice);
-    }
-
-    public static void orderPizzas(PizzaParlor food, int cheese, int pepp, int veg) {
-        for (int i = 0; i < cheese; i++) {
-            food.orderCheese();
-        }
-        for (int i = 0; i < pepp; i++) {
-            food.orderPepperoni();
-        }
-        for (int i = 0; i < veg; i++) {
-            food.orderVeggie();
-        }
-    }
-
-    public static void printOrder(PizzaParlor food){
-
-        System.out.print("# of Cheeses ordered is: " + food.getCheesePizzas() + "\n# of Pepperoni ordered is: " + food.getPeppPizzas() + "\n# of Veggie ordered is: "+ food.getVegPizzas());
-
-
-        System.out.print("\n\nRemaining supply of cheese in ounces is: " + food.getCheeseSupply() + "\nRemaining supply of pepperoni in ounces is: "+ food.getPepperoniSupply() + "\nRemaining supply of veggies in ounces is: " + food.getVeggieSupply());
-        System.out.print("\n\nRevenue is: " + String.format("$%.2f",food.getMyRevenue()) + "\nBank balance is now: " + String.format("$%.2f" , (food.getMyOrigAcctBal() + food.getMyRevenue())));
+public class PizzaTester{
+public static void main(String[] args)
+{
+PizzaParlor jimboslice = new PizzaParlor();
+orderPizzas(jimboslice, 3, 4, 3);
+printOrder(jimboslice);
 }
+public static void orderPizzas(PizzaParlor food, int cheese, int pepp, int veg)
+{
+        for(int i = 0; i < cheese; i++)
+        {
+        food.orderCheese();
+        }
+        for(int i = 0; i < pepp; i++)
+        {
+        food.orderPepperoni();
+        }
+        for(int i = 0; i < veg; i++)
+        {
+        food.orderVeggie();
+        }
+}
+    public static void printOrder(PizzaParlor food)
+    {
+    System.out.print("# of Cheeses ordered is: " + food.getCheesePizzas()
+    + "\n# of Pepperoni ordered is: " + food.getPeppPizzas() + "\n# of Veggie ordered is: "+ food.getVegPizzas());
+    System.out.print("\n\nRemaining supply of cheese in ounces is: " +
+    food.getCheeseSupply() + "\nRemaining supply of pepperoni in ounces is: "+
+    food.getPepperoniSupply() + "\nRemaining supply of veggies in ounces is: " +
+    food.getVeggieSupply() +  "\nRemaining supply of dough in ounces is: " + food.getMyDoughSupply());
+    System.out.print("\n\nRevenue is: $" + String.format("%.2f", food.getMyRevenue()) +"\nBank Balance is now: $"+ String.format("%.2f", (food.getMyOrigAcctBal()+food.getMyRevenue())));
+    }
 }
